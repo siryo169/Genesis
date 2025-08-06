4895574831e22434c8e4d55bbfc1443e466e4583
Eva Prades <Idraia87@gmail.com>
2025-08-06 10:28:12 +0000

    añadido package.json en la raiz para que se pueda trabajar en firebase Studio

--- frontend/.next/static/chunks/src_ai_flows_log-analyzer-flow_ts_e913b3f4._.js
+++ frontend/.next/static/chunks/src_ai_flows_log-analyzer-flow_ts_e913b3f4._.js
@@ -23,9 +23,9 @@ var __TURBOPACK__imported__module__$5b$project$5d2f$src$2f$ai$2f$flows$2f$log$2d
 var { g: global, __dirname } = __turbopack_context__;
 {
 __turbopack_context__.s({
-    "analyzeLogs": (()=>__TURBOPACK__imported__module__$5b$project$5d2f$src$2f$ai$2f$flows$2f$data$3a$09260d__$5b$app$2d$client$5d$__$28$ecmascript$29$__$3c$text$2f$javascript$3e$__["analyzeLogs"])
+    "analyzeLogs": (()=>__TURBOPACK__imported__module__$5b$project$5d2f$src$2f$ai$2f$flows$2f$data$3a$b5c5a8__$5b$app$2d$client$5d$__$28$ecmascript$29$__$3c$text$2f$javascript$3e$__["analyzeLogs"])
 });
-var __TURBOPACK__imported__module__$5b$project$5d2f$src$2f$ai$2f$flows$2f$data$3a$09260d__$5b$app$2d$client$5d$__$28$ecmascript$29$__$3c$text$2f$javascript$3e$__ = __turbopack_context__.i("[project]/src/ai/flows/data:09260d [app-client] (ecmascript) <text/javascript>");
+var __TURBOPACK__imported__module__$5b$project$5d2f$src$2f$ai$2f$flows$2f$data$3a$b5c5a8__$5b$app$2d$client$5d$__$28$ecmascript$29$__$3c$text$2f$javascript$3e$__ = __turbopack_context__.i("[project]/src/ai/flows/data:b5c5a8 [app-client] (ecmascript) <text/javascript>");
 var __TURBOPACK__imported__module__$5b$project$5d2f$src$2f$ai$2f$flows$2f$log$2d$analyzer$2d$flow$2e$ts__$5b$app$2d$client$5d$__$28$ecmascript$29$__$3c$locals$3e$__ = __turbopack_context__.i("[project]/src/ai/flows/log-analyzer-flow.ts [app-client] (ecmascript) <locals>");
 }}),
 "[project]/src/ai/flows/log-analyzer-flow.ts [app-client] (ecmascript)": ((__turbopack_context__) => {
--- frontend/.next/static/chunks/src_app_page_tsx_52a0d812._.js
+++ frontend/.next/static/chunks/src_app_page_tsx_52a0d812._.js
@@ -1,8 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/src_ai_flows_log-analyzer-flow_ts_9eccc525._.js",
-  "static/chunks/src_5f5ce8f5._.js",
+  "static/chunks/src_ai_flows_log-analyzer-flow_ts_6f9caea7._.js",
+  "static/chunks/src_2f7005dd._.js",
   "static/chunks/node_modules_next_b59e9fdc._.js",
   "static/chunks/node_modules_lodash_90f72504._.js",
   "static/chunks/node_modules_recharts_es6_88be8129._.js",

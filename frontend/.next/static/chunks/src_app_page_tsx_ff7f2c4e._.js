56a0dfd0dbded563cad6b31f8586dbc3dd08e86a
Eva Prades <Idraia87@gmail.com>
2025-08-07 06:56:29 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- frontend/.next/static/chunks/src_app_page_tsx_ff7f2c4e._.js
+++ frontend/.next/static/chunks/src_app_page_tsx_ff7f2c4e._.js
@@ -1,10 +1,11 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/src_ai_flows_log-analyzer-flow_ts_b67beb16._.js",
-  "static/chunks/src_3a43e945._.js",
+  "static/chunks/src_ai_flows_log-analyzer-flow_ts_4d184d6e._.js",
+  "static/chunks/src_d9770de7._.js",
   "static/chunks/node_modules_lodash_90f72504._.js",
   "static/chunks/node_modules_recharts_es6_88be8129._.js",
+  "static/chunks/node_modules_@radix-ui_react-icons_dist_react-icons_esm_ecd85ef9.js",
   "static/chunks/node_modules_@radix-ui_fb1bd267._.js",
   "static/chunks/node_modules_@floating-ui_9ec1fa39._.js",
   "static/chunks/node_modules_7a20d900._.js"

a2b2a6f234b293cfe6a482daf19a86b6fb17b199
siryo169 <79537554+siryo169@users.noreply.github.com>
2025-08-08 12:55:35 +0200

    a

--- frontend/.next/static/chunks/src_ai_flows_log-analyzer-flow_ts_55087cdf._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_ai_flows_log-analyzer-flow_ts_e913b3f4._.js"
-],
-    source: "dynamic"
-});

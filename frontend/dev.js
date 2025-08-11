0234c1d1825d18911e3431380253136c42496f17
siryo169 <79537554+siryo169@users.noreply.github.com>
2025-08-11 11:33:14 +0200

    lanzamiento front en local (#6)

--- frontend/dev.js
+++ frontend/dev.js
@@ -1,14 +1,16 @@
 // dev.js
 const { spawn } = require("child_process");
-
+const path = require("path");
 // Get port and host from command line arguments, with defaults
 const port = process.argv[2] || 3000;
 const host = process.argv[3] || "0.0.0.0";
 
+const nextBin = path.join(__dirname, "node_modules", ".bin", "next");
+
 const child = spawn(
-  "next",
+  nextBin,
   ["dev", "--turbopack", "-p", port, "-H", host],
-  { stdio: "inherit" }
+  { stdio: "inherit", shell: process.platform === "win32" }
 );
 
 child.on("close", (code) => process.exit(code));
